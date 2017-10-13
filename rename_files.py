701360ba140caa02101f51bfa891c43ebbcd18a6
queenfiona <fionaityang@gmail.com>
2017-10-13 14:56:18 +0300

    The secrect message

--- rename_files.py
+++ rename_files.py
@@ -4,6 +4,10 @@ base_dir=os.path.dirname(os.path.dirname(__file__))
 file_path=os.path.join(base_dir,'prank')
 def rename_files():
 	file_list = os.listdir('prank')
-	print(file_list)
+	print(file_list,file_path)
+	os.chdir('prank')
+	for file_name in file_list:
+		os.renames(file_name,file_name.translate(None,"0123456789"))
+	os.chdir(os.getcwd())
 
 rename_files()
